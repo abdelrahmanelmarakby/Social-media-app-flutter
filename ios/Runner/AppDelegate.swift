d473baacd0eab346cbbb99770bc17edc30b33742
Abdelrahman Mostafa Elmarakby <abdomostafa005@gmail.com>
2022-11-26 11:53:08 +0200

    Disable screenshot

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -19,4 +19,16 @@ FBSDKCoreKit.ApplicationDelegate.shared.application(
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  // <Add>
+  override func applicationWillResignActive(
+    _ application: UIApplication
+  ) {
+    self.window.isHidden = true;
+  }
+  override func applicationDidBecomeActive(
+    _ application: UIApplication
+  ) {
+    self.window.isHidden = false;
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import agora_rtc_engine
 import cloud_firestore
-import connectivity_plus
 import firebase_auth
 import firebase_core
 import firebase_messaging
@@ -27,7 +26,6 @@ import wakelock_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
