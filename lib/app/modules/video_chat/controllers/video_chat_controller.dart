03fe86e116f947f8e754bc3d2baadea0452c83c3
Abdelrahman Mostafa Elmarakby <abdomostafa005@gmail.com>
2022-11-27 12:20:26 +0200

    disable sentry and agora

--- lib/app/modules/video_chat/controllers/video_chat_controller.dart
+++ lib/app/modules/video_chat/controllers/video_chat_controller.dart
@@ -1,119 +1,115 @@
-import 'package:agora_rtc_engine/agora_rtc_engine.dart';
-import 'package:flutter/material.dart';
-import 'package:future_chat/core/global/const.dart';
 import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class VideoChatController extends GetxController {
-  @override
-  void onInit() {
-    super.onInit();
-    setupVideoSDKEngine();
-  }
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     setupVideoSDKEngine();
+//   }
 
-  var arguments = Get.arguments;
+//   var arguments = Get.arguments;
 
-  @override
-  void onClose() async {
-    await agoraEngine.leaveChannel();
-    super.onClose();
-  }
+//   @override
+//   void onClose() async {
+//     await agoraEngine.leaveChannel();
+//     super.onClose();
+//   }
 
-  int endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 30;
-  bool videoCallEstablished = false;
-  int? remoteUid; // uid of the remote user
-  bool isJoined = false; // Indicates if the local user has joined the channel
-  late RtcEngine agoraEngine; // Agora engine instance
-  bool localAudioMute = false;
-  bool localUserJoined = false;
+//   int endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 30;
+//   bool videoCallEstablished = false;
+//   int? remoteUid; // uid of the remote user
+//   bool isJoined = false; // Indicates if the local user has joined the channel
+//   late RtcEngine agoraEngine; // Agora engine instance
+//   bool localAudioMute = false;
+//   bool localUserJoined = false;
 
-  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
-      GlobalKey<ScaffoldMessengerState>(); // Global key to access the scaffold
-  Future<void> setupVideoSDKEngine() async {
-    // retrieve or request camera and microphone permissions
-    await [Permission.microphone, Permission.camera].request();
+//   final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+//       GlobalKey<ScaffoldMessengerState>(); // Global key to access the scaffold
+//   Future<void> setupVideoSDKEngine() async {
+//     // retrieve or request camera and microphone permissions
+//     await [Permission.microphone, Permission.camera].request();
 
-    //create an instance of the Agora engine
-    agoraEngine = createAgoraRtcEngine();
-    await agoraEngine.initialize(const RtcEngineContext(appId: agoraID));
+//     //create an instance of the Agora engine
+//     agoraEngine = createAgoraRtcEngine();
+//     await agoraEngine.initialize(const RtcEngineContext(appId: agoraID));
 
-    await agoraEngine.enableVideo();
+//     await agoraEngine.enableVideo();
 
-    // Register the event handler
-    agoraEngine.registerEventHandler(
-      RtcEngineEventHandler(
-        onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
-          showMessage(
-              "Local user uid:${connection.localUid} joined the channel");
+//     // Register the event handler
+//     agoraEngine.registerEventHandler(
+//       RtcEngineEventHandler(
+//         onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+//           showMessage(
+//               "Local user uid:${connection.localUid} joined the channel");
 
-          isJoined = true;
-          update();
-        },
-        onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
-          showMessage("Remote user uid:$remoteUid joined the channel");
+//           isJoined = true;
+//           update();
+//         },
+//         onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+//           showMessage("Remote user uid:$remoteUid joined the channel");
 
-          remoteUid = remoteUid;
-          update();
-        },
-        onUserOffline: (RtcConnection connection, int? remoteUid,
-            UserOfflineReasonType reason) {
-          showMessage("Remote user uid:$remoteUid left the channel");
+//           remoteUid = remoteUid;
+//           update();
+//         },
+//         onUserOffline: (RtcConnection connection, int? remoteUid,
+//             UserOfflineReasonType reason) {
+//           showMessage("Remote user uid:$remoteUid left the channel");
 
-          remoteUid = null;
-          leave();
-          update();
-        },
-      ),
-    );
-  }
+//           remoteUid = null;
+//           leave();
+//           update();
+//         },
+//       ),
+//     );
+//   }
 
-  void leave() {
-    isJoined = false;
-    remoteUid = null;
-    update();
-    agoraEngine.leaveChannel();
-    Get.back();
-  }
+//   void leave() {
+//     isJoined = false;
+//     remoteUid = null;
+//     update();
+//     agoraEngine.leaveChannel();
+//     Get.back();
+//   }
 
-  void join() async {
-    await agoraEngine.startPreview();
+//   void join() async {
+//     await agoraEngine.startPreview();
 
-    // Set channel options including the client role and channel profile
-    ChannelMediaOptions options = const ChannelMediaOptions(
-      clientRoleType: ClientRoleType.clientRoleBroadcaster,
-      channelProfile: ChannelProfileType.channelProfileCommunication,
-    );
+//     // Set channel options including the client role and channel profile
+//     ChannelMediaOptions options = const ChannelMediaOptions(
+//       clientRoleType: ClientRoleType.clientRoleBroadcaster,
+//       channelProfile: ChannelProfileType.channelProfileCommunication,
+//     );
 
-    await agoraEngine.joinChannel(
-      token: "",
-      channelId: "id:201094959669+id:201019706842+",
-      options: options,
-      uid: 0,
-    );
-    update();
-  }
+//     await agoraEngine.joinChannel(
+//       token: "",
+//       channelId: "id:201094959669+id:201019706842+",
+//       options: options,
+//       uid: 0,
+//     );
+//     update();
+//   }
 
-  Future switchCamera() async {
-    try {
-      await agoraEngine.switchCamera();
-    } catch (e) {
-      showMessage(e.toString());
-    }
-  }
+//   Future switchCamera() async {
+//     try {
+//       await agoraEngine.switchCamera();
+//     } catch (e) {
+//       showMessage(e.toString());
+//     }
+//   }
 
-  Future toggleLotcalAudioMuted() async {
-    try {
-      localAudioMute = !localAudioMute;
-      await agoraEngine.muteLocalAudioStream(localAudioMute);
-      update();
-    } catch (e) {
-      showMessage(e.toString());
-    }
-  }
+//   Future toggleLotcalAudioMuted() async {
+//     try {
+//       localAudioMute = !localAudioMute;
+//       await agoraEngine.muteLocalAudioStream(localAudioMute);
+//       update();
+//     } catch (e) {
+//       showMessage(e.toString());
+//     }
+//   }
 
-  showMessage(String message) {
-    scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
-      content: Text(message),
-    ));
-  }
+//   showMessage(String message) {
+//     scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+//       content: Text(message),
+//     ));
+//   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import agora_rtc_engine
 import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_messaging
 import firebase_storage
 import geolocator_apple
-import iris_event
-import package_info_plus
 import path_provider_macos
 import photo_manager
-import sentry_flutter
 import share_plus
 import shared_preferences_macos
 import sqflite
@@ -24,18 +20,14 @@ import url_launcher_macos
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  IrisEventPlugin.register(with: registry.registrar(forPlugin: "IrisEventPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
